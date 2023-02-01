9772d0e2d7a2c163df78360ed8769e45a094407b
SergiiMal <ser4ester@gmail.com>
2023-02-01 17:16:06 +0200

    dz_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let intervalId = setInterval(getRandomHexColor, 1000);
+
+btnStart.addEventListener('click', startChangeColor);
+btnStop.addEventListener('click', stopChangeColor);
+
+function updateBodyColor() {	
+	document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function startChangeColor(){	
+	// 
+	btnStart.disabeld = true;
+	btnStop.disabeld = false;
+	intervalId = setInterval(updateBodyColor, 1000);
+}
+
+function stopChangeColor(){
+	btnStop.disabeld = true;
+	btnStart.disabeld = false;
+	clearInterval(intervalId) 
+}
+
+
+
+// const updateBodyColor = document.body.style.backgroundColor;
 
