3919a3d1c6f3c6acb96680472e98d275f90cd050
SergiiMal <ser4ester@gmail.com>
2023-02-07 00:44:50 +0200

    +

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,18 +5,18 @@ const form = document.querySelector('.form');
 
 function onSubmitForm(e) {  
   e.preventDefault();
-  
+
   let delay = Number(e.currentTarget.delay.value);
   const step = Number(e.currentTarget.step.value);
   const amount = Number(e.currentTarget.amount.value);
-
+form.reset();
   for (let position = 1; position <= amount; position += 1) {
     createPromise(position, delay)
       .then()
           .catch();
             delay += step;
   }
-  form.reset();
+  
 }
 
 function createPromise(position, delay) {
